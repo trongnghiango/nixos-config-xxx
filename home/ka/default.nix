c581db4ea9a5c781e377f43638ffffd449676fbc
NGHIA <nghiango@gmail.com>
2025-04-17 22:52:44 +0700

    fix env in bashrc cannot modify

--- home/ka/default.nix
+++ home/ka/default.nix
@@ -16,6 +16,7 @@
     };
     initExtra = ''
       export PS1='\[\e[38;5;76m\]\u\[\e[0m\] in \[\e[38;5;32m\]\w\[\e[0m\] \$ '
+	  [[ -f "$HOME/.nprofile" ]] && source "$HOME/.nprofile"
     '';
   };
 
